b3bb846c6ef2d51203c53902d4964f9077900b82
Hknots <hknots99@gmail.com>
2025-03-28 20:09:22 +0100

    feat: add ProviderControllerTest

--- src/native-test/kotlin/no/fintlabs/provider/gateway/ProviderControllerIT.kt
+++ src/native-test/kotlin/no/fintlabs/provider/gateway/ProviderControllerIT.kt
@@ -3,4 +3,4 @@ package no.fintlabs.provider.gateway
 import io.quarkus.test.junit.QuarkusIntegrationTest
 
 @QuarkusIntegrationTest
-class ExampleResourceIT : ExampleResourceTest()
+class ProviderControllerIT : ProviderControllerTest()
--- src/test/kotlin/no/fintlabs/provider/gateway/ProviderControllerTest.kt
+++ src/test/kotlin/no/fintlabs/provider/gateway/ProviderControllerTest.kt
@@ -2,19 +2,17 @@ package no.fintlabs.provider.gateway
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
-class ExampleResourceTest {
+class ProviderControllerTest {
 
     @Test
-    fun testHelloEndpoint() {
+    fun testHeartbeat() {
         given()
-          .`when`().get("/hello")
+          .`when`().post("/heartbeat")
           .then()
              .statusCode(200)
-             .body(`is`("Hello RESTEasy"))
     }
 
 }
