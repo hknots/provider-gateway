849efcaa23e110152149771959c8646cfff89daf
Hknots <hknots99@gmail.com>
2025-03-28 20:05:36 +0100

    struct: add full, delta and delete sync

--- src/main/kotlin/no/fintlabs/provider/gateway/ProviderController.kt
+++ src/main/kotlin/no/fintlabs/provider/gateway/ProviderController.kt
@@ -1,9 +1,8 @@
 package no.fintlabs.provider.gateway
 
-import jakarta.ws.rs.GET
-import jakarta.ws.rs.POST
-import jakarta.ws.rs.Path
+import jakarta.ws.rs.*
 import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.core.Response.Status
 
 @Path("/")
 class ProviderController {
@@ -17,5 +16,41 @@ class ProviderController {
     fun heartbeat(): Response =
         Response.status(200).entity("💗").build()
 
+    @POST
+    @Path("/register")
+    fun register(): Response = TODO()
+
+    private fun handleSync(
+        domain: String,
+        pkg: String,
+        entity: String,
+        status: Status
+    ): Response {
+        return Response.status(status).build()
+    }
+
+    @POST
+    @Path("/{domain}/{package}/{entity}")
+    fun fullSync(
+        @PathParam("domain") domain: String,
+        @PathParam("package") pkg: String,
+        @PathParam("entity") entity: String
+    ): Response = handleSync(domain, pkg, entity, Status.CREATED)
+
+    @PATCH
+    @Path("/{domain}/{package}/{entity}")
+    fun deltaSync(
+        @PathParam("domain") domain: String,
+        @PathParam("package") pkg: String,
+        @PathParam("entity") entity: String
+    ): Response = handleSync(domain, pkg, entity, Status.CREATED)
+
+    @DELETE
+    @Path("/{domain}/{package}/{entity}")
+    fun deleteSync(
+        @PathParam("domain") domain: String,
+        @PathParam("package") pkg: String,
+        @PathParam("entity") entity: String
+    ): Response = handleSync(domain, pkg, entity, Status.OK)
 
 }
